43f33b269c357324c02e82b6fe59bd5bf002105e
Luc Vandal <luc@edovia.com>
2016-08-04 15:08:36 -0400

    Updated for Swift 3 b4

--- PasscodeLock/Functions.swift
+++ PasscodeLock/Functions.swift
@@ -18,7 +18,7 @@ func localizedStringFor(_ key: String, comment: String) -> String {
 
 func bundleForResource(_ name: String, ofType type: String) -> Bundle {
     
-    if(Bundle.main.pathForResource(name, ofType: type) != nil) {
+    if(Bundle.main.path(forResource: name, ofType: type) != nil) {
         return Bundle.main
     }
     
